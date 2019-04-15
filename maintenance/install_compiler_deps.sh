ddacddd7658fb34416dd7f60270e8e8cdac257f2
verhulstm <michael@terminallabs.com>
2019-04-14 20:57:23 -0500

    started work on install_compiler_deps.sh

---
+++ maintenance/install_compiler_deps.sh
@@ -0,0 +1,7 @@
+apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev
+apt install -y g++
+
+apt install -y libssl-dev
+apt install -y libssl1.0-dev
+
+apt install -y virtualenv
