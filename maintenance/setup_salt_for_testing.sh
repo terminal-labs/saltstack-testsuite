a0b07f6b620bf7044c7cf91b03b3ff6a6f74c365
Michael Verhulst <michael@terminallabs.com>
2019-04-15 13:39:53 -0500

    Update setup_salt_for_testing.sh

--- maintenance/setup_salt_for_testing.sh
+++ maintenance/setup_salt_for_testing.sh
@@ -1,5 +1,10 @@
 git clone https://github.com/saltstack/salt.git
-git clone https://github.com/saltstack/pytest-salt.git
-
 chmod 777 salt
+cd salt
+git checkout v2018.3.4
+
+cd ..
+
+git clone https://github.com/saltstack/pytest-salt.git
 chmod 777 pytest-salt
+git checkout v2018.2.2
