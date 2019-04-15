a19e708828039360ac25826b8e5dfae4d21dddd0
Michael Verhulst <michael@terminallabs.com>
2019-04-14 21:04:56 -0500

    Update install_compiler_deps.sh

--- maintenance/install_compiler_deps.sh
+++ maintenance/install_compiler_deps.sh
@@ -1,7 +1,14 @@
-apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev
+apt install -y build-essential make
 apt install -y g++
+
+apt install -y openssl lzma
 
 apt install -y libssl-dev
 apt install -y libssl1.0-dev
+apt install -y zlib1g-dev 
+apt install -y libbz2-dev 
+apt install -y libsqlite3-dev
+apt install -y lzma-dev 
+apt install -y liblzma-dev
 
 apt install -y virtualenv
