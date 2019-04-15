a46eeabacc1bf7f57fbd576acfc3c5af82778bb2
Michael Verhulst <michael@terminallabs.com>
2019-04-15 00:46:41 -0500

    Update setup_python.sh

--- maintenance/setup_python.sh
+++ maintenance/setup_python.sh
@@ -1,5 +1,6 @@
 wget https://www.python.org/ftp/python/3.4.10/Python-3.4.10.tgz
 tar -xvzf Python-3.4.10.tgz
+chmod 777 Python-3.4.10
 cd Python-3.4.10
 ./configure --with-ssl --with-zlib --with-ensurepip --prefix=/usr/local/
 make
